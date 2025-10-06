628d6924d6a49bf93c637efa61fa1458aa4fc4c4
NunezAM <arielmartinn02@gmail.com>
2025-10-06 20:55:01 -0300

    Agrego registros para prestadores, pacientes, solicitudes, reintegros, autorizaciones y recetas que se insertan al levantar los servicios.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const conectarMongoDB = require('./config/database')
 const pacienteRouter = require('./routers/pacienteRouters')
 const situacionTerapeuticaRouter = require('./routers/situacionTerapeuticaRouters')
 const solicitudRouter = require('./routers/solicitudRouters')
-
+const seeds = require('./seeds')
 require('dotenv').config()
 
 const app = express();
@@ -14,11 +14,25 @@ app.use(express.json());
 app.use('/pacientes', pacienteRouter)
 app.use('/situacionesTerapeuticas', situacionTerapeuticaRouter)
 app.use('/solicitudes', solicitudRouter)
-
+console.log(process.env.SEED)
 //Conectamos la base de datos
-conectarMongoDB()
-app.listen(PORT, ()=>{
-    console.log(`Servidor iniciado en el puerto ${PORT}`)
-    console.log('Clinica-Prestador');
-});
+async function iniciarServidor() {
+  await conectarMongoDB();
 
+  if (process.env.SEED === 'true') {
+    console.log('Insertando registros...');
+    await seeds.seedPrestadores();
+    await seeds.seedPacientes();
+    await seeds.seedSolicitudes();
+    await seeds.seedReintegros();
+    await seeds.seedAutorizaciones();
+    await seeds.seedRecetas();
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
+    console.log('Clinica-Prestador');
+  });
+}
+
+iniciarServidor()
---
+++ seeds/index.js
@@ -0,0 +1,16 @@
+const {seedPrestadores} = require('./prestadores.seed')
+const {seedPacientes} = require('./pacientes.seed')
+const {seedSolicitudes} = require('./solicitudes.seed')
+const {seedReintegros} = require('./reintegros.seed')
+const {seedAutorizaciones} = require('./autorizaciones.seed')
+const {seedRecetas} = require('./recetas.seed')
+// const Producto = require('../models/producto.model')
+
+module.exports = {
+    seedPrestadores,
+    seedPacientes,
+    seedSolicitudes,
+    seedReintegros,
+    seedAutorizaciones,
+    seedRecetas
+}
