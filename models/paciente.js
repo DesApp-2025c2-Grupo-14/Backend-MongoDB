e305f69563f32324d90b61ac6ff1bf4abe1c19fd
Lu <lucauriel.tabaka@estudiantes.unahur.edu.ar>
2025-10-10 16:15:52 -0300

    Actualizo el modelo de paciente

--- models/paciente.js
+++ models/paciente.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
 
 const pacienteSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  edad: { type: Number, required: true },
+  tipoDocumento: { type: String, required: true },
+  dni: { type: String, required: true, unique: true },
+  /* nombreCompleto: { type: String, required: true }, */
+  /* edad: { type: Number, required: true }, */
+  apellido: { type: String, required: true },
+  fechaNacimiento: {type: Date, required: true},
+  telefono: {type: String, required: true},
+  mail: {type: String, required: true},
+  direccion: {type: String, required: true},
   nroAfiliado:{type: String,required: true },
+  parentesco: {
+  type: String,
+  enum: ['Titular', 'Cónyuge', 'Hijo','Hija','Familiar a cargo'],
+  required: true
+},
+planMedico: {
+  type: String,
+  enum: ['210', '310', '410', '510', 'Bronce','Plata', 'Oro', 'Platino'],
+  required: true
+},
+
+
   situacionesTerapeuticas:[{
     type: mongoose.Schema.ObjectId,
     ref: 'SituacionTerapeutica'
