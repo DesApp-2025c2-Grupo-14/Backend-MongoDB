78a94d6dddbaef2e29a5c024d397c15150de87fa
Erik Herrera <herrik18@gmail.com>
2025-11-22 18:47:32 -0300

    Creado endpoint para obtener turnos segun la especialidad

--- routers/turnoRouters.js
+++ routers/turnoRouters.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const router = Router();
-const {obtenerTurnos} = require ('../controllers/turnoController')
+const {obtenerTurnos, turnosPorEspecialidad} = require ('../controllers/turnoController')
 
 router.get('/',obtenerTurnos);
+router.get('/:especialidad',turnosPorEspecialidad)
 
 module.exports = router;
