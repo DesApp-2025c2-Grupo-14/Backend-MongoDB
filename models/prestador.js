74f646be3519e57295abaf34e5a67e2b86d223fa
NunezAM <arielmartinn02@gmail.com>
2025-11-23 11:20:48 -0300

    Cambios en el modelo de prestador y nuevos controllers para consultar las solicitudes de los centros médicos y estadísticas

--- models/prestador.js
+++ models/prestador.js
@@ -42,6 +42,11 @@ const prestadorSchema = new mongoose.Schema({
             }
         }]
     }],
+    centroMedicoId: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Prestador',
+        default: null
+    }
 },
 {
     collection: 'prestadores'
