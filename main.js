3bcd0d4b24d60e83274bc725d3a36bfe6a105b48
sebastian castro <sebastiancastro141@gmail.com>
2025-11-24 22:02:12 -0300

    Se agregan parametro opcionales para endpoint de solicitudes pendientes

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 3001
-app.use(cors({ origin: 'http://localhost:5174' }));
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 //Rutas
